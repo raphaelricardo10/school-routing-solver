9c4cd18ce7bb268d0a601c4b18b6ca9846e1182d
raphael.goncalves <raphaelricardo10@gmail.com>
2022-07-04 22:05:17 -0300

    Created RoutingGA class to hold all parameters in Python scripts

---
+++ routingGA.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class RoutingGA:
+    def __init__(self, popSize: int = None, qtyLocations: int = None, qtyRoutes: int = None, maxGenerations: int = None, selectionK: int = None, mutationRate: float = None, distances: 'list[list[int]]' = None) -> None:
+        self.popSize = popSize
+        self.qtyLocations = qtyLocations
+        self.qtyRoutes = qtyRoutes
+        self.maxGenerations = maxGenerations
+        self.selectionK = selectionK
+        self.mutationRate = mutationRate
+        self.distances = np.array(distances, dtype=np.int32)
