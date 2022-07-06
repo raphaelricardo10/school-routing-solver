2250012b2a7adc84a03731c5dbee4cd295c3bfc6
Raphael <raphaelricardo10@gmail.com>
2022-07-06 11:02:50 -0300

    Removed the hardcoded request to use the google maps library

--- distancesAPI.py
+++ distancesAPI.py
@@ -1,95 +1,49 @@
-from __future__ import division
-from __future__ import print_function
+import googlemaps
+import os
 
 from routingGA import RoutingGA
 from GALib import GALib
 from dotenv import load_dotenv
-
-import requests
-import json
-import urllib
-import os
+from datetime import datetime as dt
 
 load_dotenv()
 
-def create_data():
-    """Creates the data."""
-    data = {}
-    data['API_key'] = os.getenv('API_KEY')
-    data['addresses'] = ['Externato+Bastos+Silva+RJ',  # depot
-                         'Rua+Mutuapira+191+RJ',
-                         'Rua+Joao+Laborde+183+RJ',
-                         'Rua+Jaime+Bitencourt+336+RJ',
-                        ]
-    return data
+class MapsAPI:
+    def __init__(self, key: str) -> None:
+        self.key = key
+        self.client = googlemaps.Client(key)
 
+    def distance_matrix(self, addresses):
+        response = self.client.distance_matrix(
+                    addresses,
+                    addresses,
+                    mode="driving",
+                    language="pt-BR",
+                    avoid="tolls",
+                    units="imperial",
+                    departure_time=dt.now(),
+                    traffic_model="optimistic",
+        )
 
-def create_distance_matrix(data):
-    addresses = data["addresses"]
-    API_key = data["API_key"]
-    # Distance Matrix API only accepts 100 elements per request, so get rows in multiple requests.
-    max_elements = 100
-    num_addresses = len(addresses)  # 16 in this example.
-    # Maximum number of rows that can be computed per request (6 in this example).
-    max_rows = max_elements // num_addresses
-    # num_addresses = q * max_rows + r (q = 2 and r = 4 in this example).
-    q, r = divmod(num_addresses, max_rows)
-    dest_addresses = addresses
-    distance_matrix = []
-    # Send q requests, returning max_rows rows per request.
-    for i in range(q):
-        origin_addresses = addresses[i * max_rows: (i + 1) * max_rows]
-        response = send_request(origin_addresses, dest_addresses, API_key)
-        distance_matrix += build_distance_matrix(response)
+        distances = []
 
-    # Get the remaining remaining r rows, if necessary.
-    if r > 0:
-        origin_addresses = addresses[q * max_rows: q * max_rows + r]
-        response = send_request(origin_addresses, dest_addresses, API_key)
-        distance_matrix += build_distance_matrix(response)
-    return distance_matrix
+        for row in response['rows']:
+            distances.append([x['distance']['value'] for x in row['elements']])
+        return distances
 
-
-def send_request(origin_addresses, dest_addresses, API_key):
-    """ Build and send request for the given origin and destination addresses."""
-    def build_address_str(addresses):
-        # Build a pipe-separated string of addresses
-        address_str = ''
-        for i in range(len(addresses) - 1):
-            address_str += addresses[i] + '|'
-        address_str += addresses[-1]
-        return address_str
-
-    request = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial'
-    origin_address_str = build_address_str(origin_addresses)
-    dest_address_str = build_address_str(dest_addresses)
-    request = request + '&origins=' + origin_address_str + '&destinations=' + \
-        dest_address_str + '&key=' + API_key
-    jsonResult = urllib.request.urlopen(request).read()
-    response = json.loads(jsonResult)
-    return response
-
-
-def build_distance_matrix(response):
-    distance_matrix = []
-    for row in response['rows']:
-        row_list = [row['elements'][j]['distance']['value']
-                    for j in range(len(row['elements']))]
-        distance_matrix.append(row_list)
-    return distance_matrix
-
-########
-# Main #
-########
 
 if __name__ == '__main__':
-    """Entry point of the program"""
-    # Create the data.
-    data = create_data()
-    addresses = data['addresses']
-    API_key = data['API_key']
+    addresses = ['Externato+Bastos+Silva+RJ',  # depot
+                    'Rua+Mutuapira+191+RJ',
+                    'Rua+Joao+Laborde+183+RJ',
+                    'Rua+Jaime+Bitencourt+336+RJ',
+                ]
 
-    distances = create_distance_matrix(data)
+    mapsAPI = MapsAPI(os.getenv('API_KEY'))
+
+    distances = mapsAPI.distance_matrix(addresses)
+
+    print(distances)
 
     routingGA = RoutingGA(popSize=200, qtyLocations=15, qtyRoutes=5,
                         maxGenerations=100, selectionK=3, mutationRate=0.65, distances=distances)
