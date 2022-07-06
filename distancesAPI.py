9a6d75d0d4cc78ee2832720949465b0880bd040c
Raphael <raphaelricardo10@gmail.com>
2022-07-06 12:24:41 -0300

    Added more locations and added the directions plot to an image

--- distancesAPI.py
+++ distancesAPI.py
@@ -31,32 +31,95 @@ class MapsAPI:
             distances.append([x['distance']['value'] for x in row['elements']])
         return distances
 
-    def directions(self, destination, source):
+    def directions(self, destination, source, waypoints: 'list[str]'):
         return self.client.directions(
             destination,
             source,
             mode="driving",
             avoid=["highways", "tolls", "ferries"],
+            waypoints=waypoints,
             units="metric",
             language="pt-BR",
             region="BR",
         )
 
+    def plot_directions(self, results):
+        marker_points = []
+        waypoints = []
+
+        #extract the location points from the previous directions function
+
+        for leg in results[0]["legs"]:
+            leg_start_loc = leg["start_location"]
+            marker_points.append(f'{leg_start_loc["lat"]},{leg_start_loc["lng"]}')
+            for step in leg["steps"]:
+                end_loc = step["end_location"]
+                waypoints.append(f'{end_loc["lat"]},{end_loc["lng"]}')
+        last_stop = results[0]["legs"][-1]["end_location"]
+        marker_points.append(f'{last_stop["lat"]},{last_stop["lng"]}')
+                
+        markers = [ "color:blue|size:mid|label:" + chr(65+i) + "|" 
+                + r for i, r in enumerate(marker_points)]
+        result_map = self.client.static_map(
+                        center = waypoints[0],
+                        scale=2, 
+                        zoom=13,
+                        size=[640, 640], 
+                        format="jpg", 
+                        maptype="roadmap",
+                        markers=markers,
+                        path="color:0x0000ff|weight:2|" + "|".join(waypoints))
+
+        return result_map
+
 
 if __name__ == '__main__':
     addresses = ['Externato+Bastos+Silva+RJ',  # depot
-                    'Rua+Mutuapira+191+RJ',
-                    'Rua+Joao+Laborde+183+RJ',
-                    'Rua+Jaime+Bitencourt+336+RJ',
+                    'Rua Mutuapira, 191, RJ',
+                    'Rua Jaime Bittencourt, 336, RJ'
+                    'Rua Espedicionário Evilásio Rocha Assis, 85, RJ',
+                    'R. João Antônio Leandro, 3473, RJ',
+                    'R. Expedicionario Manuel E De Sousa, 414 - Fazenda dos Mineiros, São Gonçalo,',
+                    'R. Heitor Rodrigues, 91 - Itaúna',
+                    'R. Laura Amélia de Souza, 239, Sao Goncalo',
+                    'R. Liborina Silva, 372 - Itaúna',
+                    'Estr. da Sapucaia, 694, Sao Goncalo',
+                    'R. Santo André, 92-242 - Itaúna',
+                    'R. Lopes de Moura, 38 - Nova Cidade',
+                    'R. Ives Ribeiro, 110 - Nova Cidade',
+                    'R. Petrópolis, 700 - Trindade',
+                    'R. Petrópolis, 1189 - Trindade',
+                    'Av. José Manna Júnior, 606, Sao Goncalo',
+                    'R. Ilhéus, 147 - Trindade',
+                    'R. Cuiabá, 661 - Trindade',
+                    'R. Itaperuna, 358 - Trindade',
+                    'R. Londrina, 104 - Trindade',
+                    'Avenida Trindade, 536 - Trindade',
+                    'R. Recife, 1609 - Trindade',
+                    'R. Uberlândia, 459 - Trindade',
+                    'R. Albino de Almeida, 85 - Trindade',
+                    'R. Barbacena, 17 - Trindade',
+                    'R. Rio de Janeiro, 751-639 - Trindade',
+                    'R. Itaocara, 82 - Trindade',
+                    'R. Guadalajara, 148-230 - Trindade',
+                    'R. Peleuzio Araújo, 59 - Mutua',
+                    'R. Manuel Pinheiro, 148 - São Miguel',
+                    'R. Peleuzio Araújo, 88 - Mutua',
+                    'R. Cel. Álvaro de Andrade, 25 - São Miguel',
+                    'R. Tales de Andrade, 448-378 - Antonina',
                 ]
 
     mapsAPI = MapsAPI(os.getenv('API_KEY'))
 
     distances = mapsAPI.distance_matrix(addresses)
-    routes = mapsAPI.directions(addresses[0], addresses[1])
+    routes = mapsAPI.directions(addresses[0], addresses[-1], addresses[1:-1])
 
-    routingGA = RoutingGA(popSize=200, qtyLocations=15, qtyRoutes=5,
-                        maxGenerations=100, selectionK=3, mutationRate=0.65, distances=distances)
+    with open('driving_route_map.jpg', 'wb') as img:
+        for chunk in mapsAPI.plot_directions(routes):
+            img.write(chunk)
+
+    routingGA = RoutingGA(popSize=50, qtyLocations=len(addresses) - 1, qtyRoutes=5,
+                        maxGenerations=100, selectionK=3, mutationRate=0.05, distances=distances)
     lib = GALib(routingGA=routingGA,
                 libPath=os.getenv('LIB_PATH'))
 
