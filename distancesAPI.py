cd4acb22da9b34807471220fdbcfc7100c09a9d9
Raphael <raphaelricardo10@gmail.com>
2022-07-06 08:03:48 -0300

    Changed the addresses

--- distancesAPI.py
+++ distancesAPI.py
@@ -16,23 +16,11 @@ def create_data():
     """Creates the data."""
     data = {}
     data['API_key'] = os.getenv('API_KEY')
-    data['addresses'] = ['3610+Hacks+Cross+Rd+Memphis+TN',  # depot
-                         '1921+Elvis+Presley+Blvd+Memphis+TN',
-                         '149+Union+Avenue+Memphis+TN',
-                         '1034+Audubon+Drive+Memphis+TN',
-                         '1532+Madison+Ave+Memphis+TN',
-                         '706+Union+Ave+Memphis+TN',
-                         '3641+Central+Ave+Memphis+TN',
-                         '926+E+McLemore+Ave+Memphis+TN',
-                         '4339+Park+Ave+Memphis+TN',
-                         '600+Goodwyn+St+Memphis+TN',
-                         '2000+North+Pkwy+Memphis+TN',
-                         '262+Danny+Thomas+Pl+Memphis+TN',
-                         '125+N+Front+St+Memphis+TN',
-                         '5959+Park+Ave+Memphis+TN',
-                         '814+Scott+St+Memphis+TN',
-                         '1005+Tillman+St+Memphis+TN'
-                         ]
+    data['addresses'] = ['Externato+Bastos+Silva+RJ',  # depot
+                         'Rua+Mutuapira+191+RJ',
+                         'Rua+Joao+Laborde+183+RJ',
+                         'Rua+Jaime+Bitencourt+336+RJ',
+                        ]
     return data
 
 
