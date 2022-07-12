93deb1d55b88e6faa6de1f4c17c60167f832e99a
Raphael <raphaelricardo10@gmail.com>
2022-07-12 11:49:22 -0300

    Added method to split the genes in a list of routes

--- routingGA.py
+++ routingGA.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 class RoutingGA:
+    def split_routes(self, genes):
+        breakpoints = [0] + genes[self.qtyLocations: ] + [self.qtyLocations]
+        routes = []
+
+        for i in range(0, len(breakpoints) - 1):
+            route = [0] + genes[breakpoints[i]: breakpoints[i+1]]
+            routes.append(route)
+
+        return routes
+
     def __init__(self, popSize: int = None, qtyLocations: int = None, qtyRoutes: int = None, maxGenerations: int = None, selectionK: int = None, mutationRate: float = None, optRate: float = None, distances: 'list[list[int]]' = None) -> None:
         self.popSize = popSize
         self.qtyLocations = qtyLocations
