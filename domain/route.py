0d62cba8afdc665f3ed2f5be0ed42af5308ea15b
Raphael Goncalves <raphaelricardo10@gmail.com>
2022-12-03 20:37:18 +0000

    ✨ feat: DDD modeling
    start of use of DDD in this project. Added the domain objects of VRP.

---
+++ domain/route.py
@@ -0,0 +1,10 @@
+from domain.stop import Stop
+from domain.vehicle import Vehicle
+
+from dataclasses import dataclass
+
+@dataclass
+class Route:
+    vehicle: Vehicle
+    stops: 'list[Stop]'
+    total_distance: float
---
+++ domain/stop.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class Stop:
+    id: int
+    usage: int
