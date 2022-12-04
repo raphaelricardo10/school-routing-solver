7be4c8b556fee06dc55f9ed073d6e8cde2d692f6
Raphael Goncalves <raphaelricardo10@gmail.com>
2022-12-04 20:49:13 +0000

    ✨ feat: Distance matrix
    added abstraction for distance matrix

--- c_interface/c_structures/c_distance_matrix.py
+++ c_interface/c_structures/c_distance_matrix.py
@@ -1,12 +1,27 @@
-import numpy as np
+from __future__ import annotations
 
-from c_interface.c_structures.c_structure import C_Structure
+import ctypes
+from c_interface.c_structures.model_list import ModelList
 
 
-class C_DistanceMatrix(C_Structure):
-    
-    def from_obj(distances: 'list[list[float]]') -> 'np.ndarray[np.float64, np.float64]':
-        return np.array(distances, dtype=np.float64)
+class C_DistanceMatrixEntry(ctypes.Structure):
+    _fields_ = [
+        ("from", ctypes.c_int32),
+        ("to", ctypes.c_int32),
+        ("distance", ctypes.c_double)
+    ]
 
-    def to_obj(distances: 'np.ndarray[float]') -> 'list[list[float]]':
-        return distances.tolist()
+    def from_obj(entry: list) -> C_DistanceMatrixEntry:
+        return C_DistanceMatrixEntry(*entry[:3])
+
+    def to_obj(self) -> list:
+        return [getattr(self, 'from'), self.to, self.distance]
+
+
+class C_DistanceMatrix(ModelList):
+
+    def from_obj(distances: 'list[list[float]]') -> 'list[C_DistanceMatrixEntry]':
+        return ModelList.from_obj(distances, C_DistanceMatrixEntry)
+
+    def to_obj(distances: 'list[C_DistanceMatrixEntry]') -> 'list[list[float]]':
+        return super().to_obj()
