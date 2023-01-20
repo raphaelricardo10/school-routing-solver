95799f407ca553dc4755e9771a11f81eca3fd5ed
Raphael Goncalves <89359384+raphaelricardo10@users.noreply.github.com>
2023-01-20 19:40:09 +0000

    🦄 refactor: updated env variable
    split the LIB_PATH variable in CPP_SOLVER_LIB and RUST_SOLVER_LIB, updating each reference into code

--- tests/fixtures/abi_test_lib.py
+++ tests/fixtures/abi_test_lib.py
@@ -20,49 +20,65 @@ from abi.structures.abi_distance_matrix import ABIDistanceMatrix, ABIDistanceMat
 class ABITestLib(SharedLibrary):
     def __init__(self, path: str):
         functions = [
-            ABIFunction('update_vehicle', [ABIVehicle], ABIVehicle),
-            ABIFunction('update_stop', [ABIStop], ABIStop),
-            ABIFunction('add_vehicle_to_array', [
-                       (ABIVehicle * 2)], (ABIVehicle * 3)),
-            ABIFunction('read_distance_matrix', [
-                       (ABIDistanceMatrixEntry * 4),  # Distances
-                       ctypes.c_size_t,  # Number of entries
-                       ctypes.c_uint32,  # Point A
-                       ctypes.c_uint32,  # Point B
-                       ], ctypes.c_double),  # Return (distance)
+            ABIFunction("update_vehicle", [ABIVehicle], ABIVehicle),
+            ABIFunction("update_stop", [ABIStop], ABIStop),
+            ABIFunction("add_vehicle_to_array", [(ABIVehicle * 2)], (ABIVehicle * 3)),
+            ABIFunction(
+                "read_distance_matrix",
+                [
+                    (ABIDistanceMatrixEntry * 4),  # Distances
+                    ctypes.c_size_t,  # Number of entries
+                    ctypes.c_uint32,  # Point A
+                    ctypes.c_uint32,  # Point B
+                ],
+                ctypes.c_double,
+            ),  # Return (distance)
         ]
 
         super().__init__(path, functions)
 
     def update_vehicle(self, vehicle: Vehicle) -> Vehicle:
-        result: ABIVehicle = self._run(
-            'update_vehicle', ABIVehicle.from_obj(vehicle))
+        result: ABIVehicle = self._run("update_vehicle", ABIVehicle.from_obj(vehicle))
 
         return result.to_obj()
 
     def update_stop(self, stop: Stop) -> Stop:
-        result: ABIStop = self._run('update_stop', ABIStop.from_obj(stop))
+        result: ABIStop = self._run("update_stop", ABIStop.from_obj(stop))
 
         return result.to_obj()
 
-    def add_vehicle(self, vehicles: 'list[Vehicle]', number_of_vehicles: int) -> 'list[Vehicle]':
+    def add_vehicle(
+        self, vehicles: "list[Vehicle]", number_of_vehicles: int
+    ) -> "list[Vehicle]":
         result = EmptyBuffer(ABIVehicle, 3)
 
-        self._run('add_vehicle_to_array', ABIVehicleList.from_obj(vehicles),
-                  number_of_vehicles, result)
+        self._run(
+            "add_vehicle_to_array",
+            ABIVehicleList.from_obj(vehicles),
+            number_of_vehicles,
+            result,
+        )
 
         return ABIVehicleList(result).to_obj()
 
-    def read_distance_matrix(self, distances: 'list[list[float]]', a: int, b: int) -> float:
-        return self._run('read_distance_matrix', ABIDistanceMatrix.from_obj(distances), len(distances), a, b)
+    def read_distance_matrix(
+        self, distances: "list[list[float]]", a: int, b: int
+    ) -> float:
+        return self._run(
+            "read_distance_matrix",
+            ABIDistanceMatrix.from_obj(distances),
+            len(distances),
+            a,
+            b,
+        )
 
 
 @fixture
 def test_lib() -> ABITestLib:
     load_dotenv()
     try:
-        lib_path = os.environ['LIB_PATH']
-        return ABITestLib(lib_path)
+        RUST_SOLVER_LIB = os.environ["RUST_SOLVER_LIB"]
+        return ABITestLib(RUST_SOLVER_LIB)
 
     except KeyError:
-        raise ValueError('Please, provide the LIB_PATH variable in .env file')
+        raise ValueError("Please, provide the RUST_SOLVER_LIB variable in .env file")
--- tests/fixtures/rust_solver_lib.py
+++ tests/fixtures/rust_solver_lib.py
@@ -10,8 +10,8 @@ from abi.libs.rust_solver_lib import RustSolverLib
 def rust_solver_lib() -> RustSolverLib:
     load_dotenv()
     try:
-        lib_path = os.environ['LIB_PATH']
+        lib_path = os.environ["RUST_SOLVER_LIB"]
         return RustSolverLib(lib_path)
 
     except KeyError:
-        raise ValueError('Please, provide the LIB_PATH variable in .env file')
+        raise ValueError("Please, provide the RUST_SOLVER_LIB variable in .env file")
