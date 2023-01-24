f2fd9e7b6df537e5693ddd6eb7c965cb6e1242ae
Raphael Goncalves <89359384+raphaelricardo10@users.noreply.github.com>
2023-01-24 14:19:05 +0000

    ✨ feat: forward args to objs
    now allows args and kwargs to forward to construction of each object

--- src/abi/structures/empty_buffer.py
+++ src/abi/structures/empty_buffer.py
@@ -1,8 +1,13 @@
+import ctypes
 from typing import TypeVar
 
-T = TypeVar("T")
+T = TypeVar("T", bound=ctypes.Structure)
 
 
 class EmptyBuffer:
-    def __new__(self, t: T, length: int) -> None:
+    def __new__(cls, t: T, length: int, *args, **kwargs) -> None:
+        if args or kwargs:
+            objs = [t(*args, **kwargs) for _ in range(length)]
+            return (t * length)(*objs)
+
         return (t * length)()
